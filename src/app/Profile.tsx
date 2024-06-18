3a76c0490ffa6054df51dfd48689583f189922ad
erik-ferreirra <erikdesouzagf@gmail.com>
2024-06-17 22:01:09 -0300

    add first component with native cn - avatar

--- src/app/Profile.tsx
+++ src/app/Profile.tsx
@@ -1,14 +1,18 @@
-import { View, Text } from "react-native"
+import { View, Image } from "react-native"
+
+import { User } from "@/components/User"
 
 interface ProfileProps {}
 
 export function Profile({ ...rest }: ProfileProps) {
   return (
-    <View
-      className="flex-1 bg-black items-center justify-center"
-      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-    >
-      <Text>Hello</Text>
+    <View className="flex-1 items-center bg-gray-700">
+      <Image
+        source={require("@/assets/banner.png")}
+        className="w-full h-52 -mb-16"
+      />
+
+      <User />
     </View>
   )
 }
