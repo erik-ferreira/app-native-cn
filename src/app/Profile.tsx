9fe1fa539b5f5ef000f2c01fa120ee0ab602a319
erik-ferreirra <erikdesouzagf@gmail.com>
2024-06-17 22:08:12 -0300

    create components Title; Badge;

--- src/app/Profile.tsx
+++ src/app/Profile.tsx
@@ -1,18 +1,23 @@
 import { View, Image } from "react-native"
 
 import { User } from "@/components/User"
+import { Skills } from "../components/Skills"
 
 interface ProfileProps {}
 
 export function Profile({ ...rest }: ProfileProps) {
   return (
-    <View className="flex-1 items-center bg-gray-700">
+    <View className="flex-1 bg-gray-700">
       <Image
         source={require("@/assets/banner.png")}
         className="w-full h-52 -mb-16"
       />
 
-      <User />
+      <View className="flex-1 px-4 pb-b">
+        <User />
+
+        <Skills />
+      </View>
     </View>
   )
 }
---
+++ src/components/Skills.tsx
@@ -0,0 +1,15 @@
+import { View } from "react-native"
+
+import { Title } from "@/components/Title"
+
+import { SKILLS } from "@/utils/skills"
+
+export function Skills() {
+  return (
+    <View>
+      <Title>Skills</Title>
+
+      <View>{/* {SKILLS.map((skill) => ())} */}</View>
+    </View>
+  )
+}
