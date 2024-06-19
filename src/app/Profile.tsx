0b812cbae8d10520d7d17b317a18cd581cc158cf
erik-ferreirra <erikdesouzagf@gmail.com>
2024-06-18 23:04:29 -0300

    complete class

--- src/app/Profile.tsx
+++ src/app/Profile.tsx
@@ -1,26 +1,43 @@
-import { View, Image } from "react-native"
+import { View, Image, Text, ScrollView, TouchableOpacity } from "react-native"
 
 import { User } from "@/components/User"
+import { Input } from "@/components/Input"
 import { Skills } from "@/components/Skills"
 import { Preferences } from "@/components/Preferences"
+import { Button } from "@/components/Button"
+import { useToast } from "@/components/Toast"
 
 interface ProfileProps {}
 
 export function Profile({ ...rest }: ProfileProps) {
+  const { toast } = useToast()
+
   return (
     <View className="flex-1 bg-gray-700">
-      <Image
-        source={require("@/assets/banner.png")}
-        className="w-full h-52 -mb-16"
-      />
+      <ScrollView>
+        <Image
+          source={require("@/assets/banner.png")}
+          className="w-full h-52 -mb-16"
+        />
 
-      <View className="flex-1 px-4 pb-b">
-        <User />
+        <View className="flex-1 px-4 pb-b">
+          <User />
 
-        <Skills />
+          <Skills />
 
-        <Preferences />
-      </View>
+          <Preferences />
+
+          <View className="w-full mt-6 flex-1">
+            <Input label="Company" placeholder="Company" inputClasses="mb-6" />
+
+            <Button
+              label="Save"
+              className="border border-green-500"
+              onPress={() => toast("Save")}
+            />
+          </View>
+        </View>
+      </ScrollView>
     </View>
   )
 }
---
+++ src/components/Input.tsx
@@ -0,0 +1,34 @@
+import { forwardRef } from "react"
+import { Text, TextInput, View } from "react-native"
+
+import { colors } from "@/styles/colors"
+
+import { cn } from "../lib/utils"
+
+export interface InputProps
+  extends React.ComponentPropsWithoutRef<typeof TextInput> {
+  label?: string
+  labelClasses?: string
+  inputClasses?: string
+}
+const Input = forwardRef<React.ElementRef<typeof TextInput>, InputProps>(
+  ({ className, label, labelClasses, inputClasses, ...props }, ref) => (
+    <View className={cn("flex flex-col gap-2.5", className)}>
+      {label && (
+        <Text className={cn("text-white text-base font-bold", labelClasses)}>
+          {label}
+        </Text>
+      )}
+      <TextInput
+        placeholderTextColor={colors.gray[400]}
+        className={cn(
+          inputClasses,
+          "py-2.5 px-4 rounded-lg text-white bg-gray-600"
+        )}
+        {...props}
+      />
+    </View>
+  )
+)
+
+export { Input }
